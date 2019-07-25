1a28cb5eb6ab78187a14ca00fc0306af1c82785f
Mattia Ziulu <mziulu@gmail.com>
2019-07-25 11:02:47 +0200

    Add reset_db script to help with database seeding

---
+++ reset_db.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+
+mix ecto.drop
+mix ecto.create
+mix ecto.migrate
+mix run "priv/repo/seeds.exs"
+
--- start_postgres.sh
+++ start_postgres.sh
@@ -2,3 +2,6 @@
 
 # run container
 docker container run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -v football-server-pgsql:/var/lib/postgresql/data --rm postgres:11-alpine
+
+# start psql
+#docker container exec -it postgres psql -U postgres
